9c6b5233b004bac5172a6de74111eb6b05c4c221
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2022-08-05 00:06:17 +0300

    depoly

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,6 @@ REST_FRAMEWORK = {
 JWT_SECRET_KEY = (
     "JWT_SECRET_KEYJWT_SECRET_KEYJWT_SECRET_KEYJWT_SECRET_KEYJWT_SECRET_KEY"
 )
+
+
+django_heroku.settings(locals())
