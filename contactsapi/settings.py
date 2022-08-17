45e72e50e92df77657a244bcca42bbfef24fa240
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2022-08-18 00:19:14 +0300

    hosting

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+# STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# Default primary key field type
+# Default primary key field typess
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
