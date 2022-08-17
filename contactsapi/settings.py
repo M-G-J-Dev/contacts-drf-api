0145e396fe7697528e2e797d591a7fcce4901666
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2022-08-17 23:53:18 +0300

    hosting

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,5 +153,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 
-
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
